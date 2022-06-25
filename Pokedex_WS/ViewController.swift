3f1c5dee7421bd2536385b671c8a8a3ad2210f93
Wallace Santos <wallace_bcc@hotmail.com>
2022-06-25 02:14:31 -0300

    Initial Commit

---
+++ Pokedex_WS/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Pokedex_WS
+//
+//  Created by Wallace Santos on 25/06/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
